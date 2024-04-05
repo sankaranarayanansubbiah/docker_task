17438fd83479468de0a7eba251efcb35578047b6
sankaranarayanan <163542734+sankaranarayanansubbiah@users.noreply.github.com>
2024-04-05 22:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-# Define your basic details
-basic_details = {
-    'name': 'sankar',
-    'age': 28,
-    'course': 'Devops',
-    'location': 'chennai'
-}
+def colorful_header(text):
+    # ANSI escape sequences for colors
+    HEADER = '<h1 style="color: #00ff00;">'
+    END = '</h1>'
+    return f"{HEADER}{text}{END}"
 
-# Route to display basic details
 @app.route('/')
-def display_details():
-    return render_template('index.html', details=basic_details)
+def index():
+    header = colorful_header("Welcome....!")
+    details = '<h2>Here is My Basic Details</h2><p>Name: Narasimmamoorthy</p><p>Email: bala21.10moorthy@gmail.com</p><p>Role :DevOps</p>'
+    return f"{header}\n{details}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=4000)
